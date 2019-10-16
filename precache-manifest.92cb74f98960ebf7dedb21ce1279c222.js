042a361b45e5fc4f5388c6573b1e46d1c46dc80a
Krystian Bieniek <krystekk2013@gmail.com>
2019-10-16 17:44:34 +0200

    Updates

---
+++ precache-manifest.92cb74f98960ebf7dedb21ce1279c222.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb7d2c484ee5c988345b",
+    "url": "/Random-user-fetch-click/static/js/runtime~main.eb7d2c48.js"
+  },
+  {
+    "revision": "1218709b819170740b42",
+    "url": "/Random-user-fetch-click/static/js/main.1218709b.chunk.js"
+  },
+  {
+    "revision": "2eb1efbf424eb56e824f",
+    "url": "/Random-user-fetch-click/static/js/1.2eb1efbf.chunk.js"
+  },
+  {
+    "revision": "1218709b819170740b42",
+    "url": "/Random-user-fetch-click/static/css/main.a3e4d664.chunk.css"
+  },
+  {
+    "revision": "dac0e663511b56fe97cf89ddd188122e",
+    "url": "/Random-user-fetch-click/index.html"
+  }
+];
